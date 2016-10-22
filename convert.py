b3566bb4a10f73dfa61414dc978a34dac9b85b48
michaelasper <asper@cs.utexas.edu>
2016-10-22 13:34:49 -0500

    Added STFT converter + testing

--- convert.py
+++ convert.py
@@ -1,3 +1,12 @@
 from pydub import AudioSegment
+import stft
+import scipy.io.wavfile as wav
+
 sound = AudioSegment.from_mp3("Monkey_Warhol_-_01_-_Everything_Starts_with_an_E.mp3")
-sound.export("song1", format="wav")
+sound.export("song1.wav", format="wav")
+
+fs,audio = wav.read('song1.wav')
+
+specgram = stft.spectrogram(audio)
+
+print(specgram)
