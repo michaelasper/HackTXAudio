0c64476e0623bfabbd5312b6d803ebee1ee6b0cd
michaelasper <asper@cs.utexas.edu>
2016-11-07 14:28:03 -0600

    Cleaning Up - Reorganizing

--- test.py
+++ test.py
@@ -1,1 +1,4 @@
+#Test commit to make
+#sure conection and git was working
+#at the hackathon and on all of our computers
 print("Hello World")
--- writeWav.py
+++ writeWav.py
@@ -1,4 +1,9 @@
 import scipy.io.wavfile;
 
+#Preprocessing the data
+#We have all the wavFiles to numpy arrays for us to
+#preprocess on multiple computers while we trained
+#on the much faster computer at the time
+
 def convertNumpyToWavFile(filename, rate, data):
     wavfile.write(filename, rate, data);
